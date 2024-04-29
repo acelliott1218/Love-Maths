7a4f57ee3199b3dfef3a928daf9698b44979d1ef
Chris Elliott <acelliott1218@gmail.com>
2024-04-29 15:15:42 +0000

    it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,58 @@
-// wait for dom to load before running game//
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded",function() {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-    for (let button of buttons){
-        button.addEventListener("click",function(){
-            if (this.getAttribute("data-type") === "submit"){
-                alert("You clicked submit!")
-            }else{
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
             }
-        })
+        });
     }
-})
+
+});
+
 /**
- * The main game "loop" called when the script is first loaded
+ * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
-    let num1 = Math.floor(Math.random() * 25 + 1)
-    let num2 = Math.floor(Math.random() * 25 + 1)
-}
+function runGame() {
 
-function checkAnswer(){
-
-}
-
-function calculateCorrectAnswer(){
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
-function incrementScore(){
+function checkAnswer() {
 
 }
 
-function incrementWrongAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
-function displayAdditionQuestion(){
+function incrementScore() {
 
 }
 
-function displaySubtractQuestion(){
+function incrementWrongAnswer() {
 
 }
 
-function displayMultiplyQuestion(){
+function displayAdditionQuestion(operand1, operand2) {
 
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
 }
